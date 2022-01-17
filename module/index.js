eb0a63c17046d6360e5a601bbcd481084546c12c
ldh3907 <ldh165163@gmail.com>
2022-01-17 22:30:32 +0900

    2022-01-17

---
+++ module/index.js
@@ -0,0 +1,9 @@
+//원 넓이 구하는 공식
+// PI 정의
+// 공식을 통해 결과 얻기
+
+// const { getCircleArea } = require("./mathUtil");
+import { getCircleArea } from "./mathUtil";
+
+const result = getCircleArea(2);
+console.log(result);
