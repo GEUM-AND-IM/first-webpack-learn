72a445af9554785329515701324c9f055f36f9bc
ldh3907 <ldh165163@gmail.com>
2022-01-19 20:09:58 +0900

    2022-01-19

--- webpack-practice/webpack.config.js
+++ webpack-practice/webpack.config.js
@@ -1,10 +1,13 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   entry: "./index.js",
   output: {
-    filename: "bundle.js",
+    filename: "[name].[chunkhash].js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
@@ -12,11 +15,14 @@ module.exports = {
       {
         test: /\.css$/i,
         use: [
+          //   {
+          //     loader: "style-loader",
+          //     options: {
+          //       injectType: "singletonStyleTag",
+          //     },
+          //   },
           {
-            loader: "style-loader",
-            options: {
-              injectType: "singletonStyleTag",
-            },
+            loader: MiniCssExtractPlugin.loader,
           },
           {
             loader: "css-loader",
@@ -26,12 +32,54 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.hbs$/,
+        use: ["handlebars-loader"],
+      },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./template.html",
+    new MiniCssExtractPlugin({
+      filename: "[contenthash].css",
     }),
+    new CssMinimizerPlugin({
+      test: /\.css$/g,
+      minimizerOptions: {
+        preset: [
+          "default",
+          {
+            discardComments: { removeAll: true },
+          },
+        ],
+      },
+    }),
+    new HtmlWebpackPlugin({
+      title: "Webpack",
+      template: "./template.hbs",
+      meta: {
+        viewport: "width=device-width, initial-scale=1.0",
+      },
+      minify: {
+        collapseWhitespace: true,
+        useShortDoctype: true,
+        removeScriptTypeAttributes: true,
+      },
+    }),
+    new CleanWebpackPlugin(),
   ],
+  optimization: {
+    runtimeChunk: {
+      name: "runtime",
+    },
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "venders",
+          chunks: "all",
+        },
+      },
+    },
+  },
   mode: "none",
 };
