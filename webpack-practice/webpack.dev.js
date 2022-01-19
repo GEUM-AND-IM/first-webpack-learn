c39dd5c76be08ab4f39c6319db2dabf0ca8579a6
ldh3907 <ldh165163@gmail.com>
2022-01-20 00:11:28 +0900

    2022-01-20

--- webpack-practice/webpack.dev.js
+++ webpack-practice/webpack.dev.js
@@ -3,6 +3,25 @@ const common = require("./webpack.common");
 
 const config = {
   mode: "development",
+  devServer: {
+    open: true,
+    client: {
+      overlay: true,
+    },
+    historyApiFallback: {
+      rewrites: [
+        {
+          from: /^\/subpage$/,
+          to: "subpage.html",
+        },
+        {
+          from: /./,
+          to: "404.html",
+        },
+      ],
+    },
+    port: 3333,
+  },
 };
 
 module.exports = merge.merge(common, config);
